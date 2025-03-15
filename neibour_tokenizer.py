3efe5d3985f01b205b811a2c3f8bc65e2dfbd9e5
ArchBase <147291458+ArchBase@users.noreply.github.com>
2025-03-15 09:13:17 +0530

    moving to structured approach

--- neibour_tokenizer.py
+++ neibour_tokenizer.py
@@ -32,9 +32,40 @@ def generate_word_details(sentences):
 
 
 def tokenize(word_details):
-    for key, word in word_details.items():
-        if word["left"].isEmpty():
-            
+
+    def create_best_phrase_combination_for_word(key, word_details):
+
+        def create_left_phrase(key, word_details):
+            left_word = word_details[key]["left"][0]
+            left_phrase = ""
+            if len(word_details[left_word]['right']) == 1 and len(word_details[left_word]['right'][0] == key):
+                left_phrase = left_word + " "
+                return left_phrase
+
+            elif len(word_details[left_word]['right']) > 1:
+                left_phrase = ""
+                return left_phrase
+
+
+        left_words = word_details[key]["left"]
+        right_words = word_details[key]["right"]
+
+        if (left_words.isEmpty() or len(left_words) > 1) and (right_words.isEmpty() or len(right_words) > 1):
+            # individual word is the token
+            return key
+
+        elif len(left_words) == 1:
+            # there is left dependency
+            create_left_phrase(key, word_details)
+
+
+    word_to_token = {}
+    token_index = 0
+    for key in word_details.items():
+        word_to_token[create_best_phrase_combination_for_word(key, word_details)] = token_index
+        token_index += 1
+
+
 
 
 print(tokenize(generate_word_details(read_sentences("d.json"))))
